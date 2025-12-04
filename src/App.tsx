e3d702e22fd4b4753e46d1ce5d6aef3bacb654cb
2192375d <173829746yzpq@gmail.com>
2025-12-04 16:45:48 -0500

    feat: changed some colours

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import PostsNavBar from './components/postsNavBar'
-import NavBar from './components/navBar'
+import NavBar from '@components/navBar'
 import Home from './pages/home'
 import About from './pages/about'
 import Posts from './pages/posts'
