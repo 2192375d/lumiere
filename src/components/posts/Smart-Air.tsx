f9a72a68f7f39a8b06ce05ca645fc64da2dcd4fa
2192375d <173829746yzpq@gmail.com>
2025-12-04 11:06:20 -0500

    started smart air

---
+++ src/components/posts/Smart-Air.tsx
@@ -0,0 +1,30 @@
+export default function SmartAir() {
+  return (
+    <section>
+      An Android App dedicated to help kid understand and handle Asthma.
+      <br />
+      <ul>
+        <li><a href="https://github.com/Kyaw-Thiha/b07-project">github repo</a></li>
+      </ul>
+      <h3>tools/methodologies used throughout the project</h3>
+      This project is complete through a team of 5 people, utilizing the following tools/
+      <ul>
+        <li>Firebase Authentication</li>
+        <li>Firebase Realtime Database</li>
+        <li>Android Studio/Android SDK (java + xml)</li>
+        <li>Model View Process</li>
+        <li>Scrum using Jira</li>
+      </ul>
+      <h3>contributors</h3>
+      All of these people (including me) worked really hard for this project! Checkout their Github if you are interested.
+      <br />
+      <ul>
+        <li>https://github.com/2192375d</li>
+        <li>https://github.com/Kyaw-Thiha</li>
+        <li>https://github.com/najjalowe</li>
+        <li>https://github.com/Tina-wanjun</li>
+        <li>https://github.com/J0thame</li>
+      </ul>
+    </section>
+  )
+}
